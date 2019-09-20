a0c2ff31d2a339ca73b30ea601f9fea7121f950a
Chris Lamb <christopherwlamb@gmail.com>
2019-09-20 17:07:39 -0400

    global css

---
+++ pages/resume.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="container">
+    <div>
+      <h1 class="title">
+        Resume
+      </h1>
+      <h2 class="subtitle">
+        Step into my portfolio...
+      </h2>
+      <div class="links">
+        <nuxt-link to="resume" class="button--green">Click Here</nuxt-link>
+      </div>
+    </div>
+  </div>
+</template>
+
+<style>
+
+</style>
