89fce48c83267a2f508065593d8fb107012cb422
Chris Lamb <christopherwlamb@gmail.com>
2019-09-20 17:05:11 -0400

    develop push

--- pages/index.vue
+++ pages/index.vue
@@ -3,26 +3,13 @@
     <div>
       <logo />
       <h1 class="title">
-        lamb-portfolio
+        Hi, I'm Chris.
       </h1>
       <h2 class="subtitle">
-        My superb Nuxt.js project
+        Step into my portfolio...
       </h2>
       <div class="links">
-        <a
-          href="https://nuxtjs.org/"
-          target="_blank"
-          class="button--green"
-        >
-          Documentation
-        </a>
-        <a
-          href="https://github.com/nuxt/nuxt.js"
-          target="_blank"
-          class="button--grey"
-        >
-          GitHub
-        </a>
+        <nuxt-link to="resume" class="button--green">Click Here</nuxt-link>
       </div>
     </div>
   </div>
@@ -39,34 +26,5 @@ export default {
 </script>
 
 <style>
-.container {
-  margin: 0 auto;
-  min-height: 100vh;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  text-align: center;
-}
 
-.title {
-  font-family: 'Quicksand', 'Source Sans Pro', -apple-system, BlinkMacSystemFont,
-    'Segoe UI', Roboto, 'Helvetica Neue', Arial, sans-serif;
-  display: block;
-  font-weight: 300;
-  font-size: 100px;
-  color: #35495e;
-  letter-spacing: 1px;
-}
-
-.subtitle {
-  font-weight: 300;
-  font-size: 42px;
-  color: #526488;
-  word-spacing: 5px;
-  padding-bottom: 15px;
-}
-
-.links {
-  padding-top: 15px;
-}
 </style>
