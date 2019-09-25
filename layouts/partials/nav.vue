f9e2fedb8e6d3a786ec35e32902e9f261087c6af
Chris Lamb <christopherwlamb@gmail.com>
2019-09-25 17:51:21 -0400

    partial's added

---
+++ layouts/partials/nav.vue
@@ -0,0 +1,48 @@
+<template>
+  <header>
+    <nuxt-link to="/" class="logo">CL</nuxt-link>
+    <nav>
+      <div class="nav-link-holder">
+          <div class="nav-link"><nuxt-link to="/">Home</nuxt-link></div>
+          <div class="nav-link"><nuxt-link to="resume">Resume</nuxt-link></div>
+          <div class="nav-link"><nuxt-link to="portfolio">Portfolio</nuxt-link></div>
+          <div class="nav-link"><nuxt-link to="contact">Contact</nuxt-link></div>
+      </div>
+    </nav>
+  </header>
+</template>
+<script>
+export default {
+}
+</script>
+<style>
+  header {
+      background: rgb(0, 000, 000);
+      display: grid;
+      grid-template-columns: repeat(2,auto);
+  }
+  .logo:hover, .nav-link a:hover {
+      color: #41b883;
+      text-decoration: none;
+  }
+  .logo, .nav-link a {
+      padding: 1em 2em;
+      display: block;
+      text-transform: uppercase;
+      text-decoration: none;
+      font-weight: bold;
+      color: white;
+      font-size: .9em;
+  }
+  nav {
+      justify-self: right;
+  }
+  .nav-link {
+      display: inline;
+  }
+  .nav-link a {
+      padding: 1em 2em;
+      display: inline-block;
+      background: rgba(0,0,0,0.1);
+  }
+</style>
